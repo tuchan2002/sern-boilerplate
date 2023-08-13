7c3bd8c1ec74066fdb9e714be03caca8445d5410
tuchan2002 <trananhtu171002@gmail.com>
2023-08-13 21:15:45 +0700

    Untrack files in .gitignore

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
 function App() {
-    const [greeting, setGreeting] = useState('');
-
-    useEffect(() => {
-        const fetchDataTest = async () => {
-            const response = await axios.get('http://localhost:8080');
-            setGreeting(response.data.message);
-        };
-        fetchDataTest();
-    }, []);
-
     return (
         <div>
             <p>
-                {greeting}
+                Hello World
             </p>
         </div>
     );
